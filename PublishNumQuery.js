675babcb17a56a7ed1785583361806088230dcee
cuiyl <cuiyl@ewan.cn>
2018-02-10 11:12:29 +0800

    修复bug，增加查询结果内容

--- PublishNumQuery.js
+++ PublishNumQuery.js
@@ -9,17 +9,27 @@ const puppeteer = require('puppeteer');
     await page.goto ('http://www.sapprft.gov.cn/sapprft/zongshu/serviceList2.shtml?FileName=' + gameName);
     await page.waitFor(3000);
 
-    for (i=2;i<10;i++) {
-        let name_tdsel = '#ajaxElement_1_702 > table > tbody > tr:nth-child(' + i + ') > td:nth-child(1) > a';
-        const name = await page.$eval(name_tdsel, a => a.innerText);
+    try {
+        for (i=2;i<10;i++) {
+            let name_tdsel = '#ajaxElement_1_850 > table > tbody > tr:nth-child(' + i + ') > td:nth-child(1) > a';
+            let pubCom_tdsel = '#ajaxElement_1_850 > table > tbody > tr:nth-child(' + i + ') > td:nth-child(2)';
+            let optCom_tdsel = '#ajaxElement_1_850 > table > tbody > tr:nth-child(' + i + ') > td:nth-child(3)';
+            let pubNum_tdsel = '#ajaxElement_1_850 > table > tbody > tr:nth-child(' + i + ') > td:nth-child(4)';
+            const name = await page.$eval(name_tdsel, a => a.innerText);
+            const pubCom = await page.$eval(pubCom_tdsel, a => a.innerText);
+            const optCom = await page.$eval(optCom_tdsel, a => a.innerText);
+            const pubNum = await page.$eval(pubNum_tdsel, a => a.innerText);
 
-        if (name == gameName) {
-            console.log(name + "有版号！");
-            break;
+            if (name == gameName) {
+                console.log(name + "  " + pubCom + "  " + optCom + "  " + pubNum);
+                break;
+            }
+            else{
+                continue;
+            }
         }
-        else{
-            continue;
-        }
+    } catch (e) {
+        console.log(gameName + '没有版号');
     }
     browser.close();
 })();
